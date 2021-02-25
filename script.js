91e4abaae39269e7016c96122cc5cb0efe7f1cf8
gabrielCesarino <gahnrq@hotmail.com>
2021-02-24 22:56:14 -0300

    add enterPledge into modal

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const backButton = document.querySelector(".backButton");
 const modalBackground = document.querySelector(".modalBackground");
 const closeModalButton = document.querySelector(".closeButton");
+const bookmarkButton = document.querySelector(".bookmarkButton");
 
 backButton.addEventListener("click", (e) => {
   modalBackground.classList.toggle("active");
@@ -8,4 +9,12 @@ backButton.addEventListener("click", (e) => {
 
 closeModalButton.addEventListener("click", () => {
   modalBackground.classList.remove("active");
+});
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmarked");
+
+  bookmarkButton.classList.contains("bookmarked")
+    ? (bookmarkButton.querySelector("p").innerText = "Bookmarked")
+    : (bookmarkButton.querySelector("p").innerText = "Bookmark");
 });
